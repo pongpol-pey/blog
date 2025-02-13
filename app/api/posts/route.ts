d129239590ebe988be0adcd93f9973c058c39145
Pongpol Worrapruekpisuth <>
2025-02-13 13:21:52 +0700

    Rollback route.ts in /api/posts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,9 +1,8 @@
-// export const runtime = "edge";
-
 import { NextResponse } from "next/server";
 import { getPosts } from "../../get-posts";
 
 export const dynamic = "force-dynamic";
+export const runtime = "edge";
 
 export async function GET() {
   return NextResponse.json(await getPosts());
