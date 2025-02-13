d0698aae947c1ef4bedd30f409098d9d9ea561d3
Pongpol Worrapruekpisuth <>
2025-02-13 13:08:23 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = withMDX({
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   experimental: {
     mdxRs: true,
+    runtime: "edge",
   },
   swcMinify: true,
   reactStrictMode: true,
