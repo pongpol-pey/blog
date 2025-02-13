9327d9ac1b92c9362fa3dd5c97054f2c8f6b2424
Pongpol Worrapruekpisuth <>
2025-02-13 12:54:47 +0700

    Update route.ts in /api/posts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,7 +1,8 @@
+export const runtime = "edge";
+
 import { NextResponse } from "next/server";
 import { getPosts } from "../../get-posts";
 
-export const runtime = 'edge';
 export const dynamic = "force-dynamic";
 
 export async function GET() {
