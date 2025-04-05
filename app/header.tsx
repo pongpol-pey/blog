8b0880eec9cf28d813d7d4b9b5152bfc5f144f62
Pongpol Woraprukpisut <pongpol.pey@gmail.com>
2025-04-05 09:53:50 +0700

    Adjust site hierachy and change site icon

--- app/header.tsx
+++ app/header.tsx
@@ -11,10 +11,10 @@ export function Header() {
         <ThemeToggle />
 
         <Link
-          href="/about"
+          href="/blog"
           className="inline-flex hover:bg-gray-200 dark:hover:bg-[#313131] active:bg-gray-300 dark:active:bg-[#242424] rounded-sm p-2 transition-[background-color]"
         >
-          About
+          Blog
         </Link>
         {/* <a
           href="https://twitter.com/rauchg"
